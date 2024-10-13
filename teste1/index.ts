363cf87327a121deeebbe58263e9d99949b34674
RafaelSales93 <165732973+RafaelSales93@users.noreply.github.com>
2024-10-13 11:30:21 -0300

    Delete teste1 directory

--- teste1/index.ts
+++
@@ -1,7 +0,0 @@
-import * as promptSync from 'prompt-sync'
-var prompt = promptSync()
-
-var numero: number = parseInt (prompt('Digite um numero'))
-if (numero % 2 === 0 ? 'par '){
-console.log ( "esse numero e Par")
-}
