406f4317abc8951beac71ddf8758c9fe5aaa2348
RafaelSales93 <fco.rsales@gmail.com>
2024-10-13 12:01:07 -0300

    retificaçao

--- Teste1-ImparOuPar/index.js
+++ Teste1-ImparOuPar/index.js
@@ -3,17 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var promptSync = require("prompt-sync");
 var prompt = promptSync();
 function PaRouImpar(numero) {
-    // Verifica se o número é par ou ímpar
     var parOuImpar = numero % 2 === 0 ? 'par' : 'ímpar';
-    // Verifica se o número é positivo ou negativo
     var positivoOuNegativo = numero >= 0 ? 'positivo' : 'negativo';
-    // Retorna o resultado
     return "O n\u00FAmero ".concat(numero, " \u00E9 ").concat(parOuImpar, " e ").concat(positivoOuNegativo, ".");
 }
-// Função para receber entrada do usuário
 function receberNumero() {
-    var entrada = prompt("Digite um número:"); // Recebe a entrada do usuário
-    var numero = Number(entrada); // Converte a entrada para número
+    var entrada = prompt("Digite um número:");
+    var numero = Number(entrada);
     if (isNaN(numero)) {
         console.log("Por favor, insira um número válido.");
     }
@@ -22,5 +18,4 @@ function receberNumero() {
         console.log(resultado);
     }
 }
-// Chama a função para iniciar o processo
 receberNumero();
