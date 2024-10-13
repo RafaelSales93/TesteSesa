db3f4715b2ad0b0d62476c9b2f8acbdcdcbc95db
RafaelSales93 <fco.rsales@gmail.com>
2024-10-13 12:07:14 -0300

    melhoria de codigo

--- Teste2-Calcular-1a100/index.ts
+++ Teste2-Calcular-1a100/index.ts
@@ -6,11 +6,10 @@ function calculaSoma(): void {
     let soma = 0;
     for (let i = 1; i <= 100; i++) {
         soma += i;
-    console.log(`Somando ${i}: a soma atual é ${soma}`);
     }
     console.log(`A soma total dos números de 1 a 100 é: ${soma}`);
 }
-function exibirSoma(): void {
+function exibirSoma(){
     const resultado = calculaSoma();
 }
 
